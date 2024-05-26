d2f89cf6c37d9eaf603c728371242de815651bd7
Rokku <quocloctran357@gmail.com>
2024-05-26 11:29:58 +0700

    Initial Commit

---
+++ RokMusic/RokMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  RokMusicApp.swift
+//  RokMusic
+//
+//  Created by Quốc Lộc on 26/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RokMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
